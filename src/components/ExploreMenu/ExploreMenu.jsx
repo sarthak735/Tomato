0e6a2a4d034db8559d7c09d8add1b4a1e26fd530
Kunwar Sarthak Singh <kuwarsarthaksingh735@gmail.com>
2024-05-06 19:01:23 +0530

    Made the Explore menu component

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -3,8 +3,20 @@ import { menu_list } from '../../assets/assets'
 
 const ExploreMenu = () => {
   return (
-    <div>
-      
+    <div className=' flex flex-col gap-[20px]'>
+      <h1 className=' text-[#262626] font-medium text-[30px]'>Explore our menu</h1>
+      <p className=' max-w-[60%] text-neutral-800 font-normal'>Choose froma diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culnary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+      <div className=' flex justify-between items-center gap-[30px] text-center my-[20px] mx-[0px] overflow-x-scroll no-scrollbar'>
+        {menu_list.map((item,index) =>{
+            return (
+                <div key={index}>
+                    <img src={item.menu_image} alt='' className=' w-[7.5vw] min-w-[80px] cursor-pointer rounded-[50%] transition-all'/>
+                    <p className=' mt-[10px] text-[#747474] text-[max(1.4vw,16px)] cursor-pointer'>{item.menu_name}</p>
+                </div>
+            )
+        })}
+      </div>
+      <hr className=' my-[10px] mx-[0px] h-[2px] bg-[#e2e2e2] border-none'/>
     </div>
   )
 }
